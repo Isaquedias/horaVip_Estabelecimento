31e993875c135028ed6674a2096d600242f04ee3
Isaque Dias <ysaquedias@gmail.com>
2018-09-24 10:48:13 -0300

    Atualizaçãode segurança

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,91 +1,79 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { MenuModel } from '../model/Menu.model'
-import { Storage } from '@ionic/storage';
-
+import { Component, ViewChild } from "@angular/core";
+import { Nav, Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { MenuModel } from "../model/Menu.model";
+import { Storage } from "@ionic/storage";
+import { FirebaseInit } from "./firebaseInit";
 import firebase from "firebase";
-import "firebase/firestore"
+import "firebase/firestore";
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav
+  @ViewChild(Nav)
+  nav: Nav;
 
-  public rootPage: string
-  public pages: Array<MenuModel>
+  public rootPage: string;
+  public pages: Array<MenuModel>;
 
   constructor(
     platform: Platform,
     statusBar: StatusBar,
     splashScreen: SplashScreen,
-    public storage: Storage) {
+    public storage: Storage
+  ) {
+    new FirebaseInit();
 
-    firebase.initializeApp({
-      apiKey: "AIzaSyB6augiqsR9miHMV-M8FBjHhI9daPnKeJg",
-      authDomain: "priapp-9d632.firebaseapp.com",
-      databaseURL: "https://priapp-9d632.firebaseio.com",
-      projectId: "priapp-9d632",
-      storageBucket: "priapp-9d632.appspot.com",
-      messagingSenderId: "365829003106"
-    });
-
-    firebase.firestore().settings({ timestampsInSnapshots: true })
-
-    //Menu 
+    //Menu
     this.pages = [
       { title: "Meus Dados", page: "MeusDadosPage", icon: "" },
       { title: "Configuração", page: "ConfigPage", icon: "" },
       { title: "Suporte", page: "SuportePage", icon: "" },
       { title: "Sobre", page: "SobrePage", icon: "" }
-    ]
+    ];
 
     platform.ready().then(() => {
-
-
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
 
-      this.isOnline().then((data) => {
+      this.isOnline().then(data => {
         if (data) {
-          this.rootPage = "MyTabPage"
+          this.rootPage = "MyTabPage";
         } else {
-          this.rootPage = "ApresentacaoPage"
+          this.rootPage = "ApresentacaoPage";
         }
-      })
+      });
     });
   }
 
-  public irPage(page: { title: string, page: string }) {
-    this.nav.push(page.page)
+  public irPage(page: { title: string; page: string }) {
+    this.nav.push(page.page);
   }
 
-  isOnline() : Promise<boolean> {
+  isOnline(): Promise<boolean> {
     return new Promise<boolean>((res, err) => {
-      firebase.auth().onAuthStateChanged((user) => {
+      firebase.auth().onAuthStateChanged(user => {
         if (user !== null) {
-          res(true)
+          res(true);
         } else {
-          res(false)
+          res(false);
         }
-        err('err')
-      })
-    })
+        err("err");
+      });
+    });
   }
 
   async signOut() {
     try {
-      await this.nav.setRoot("LoginPage")
-      await this.storage.clear()
-      await firebase.auth().signOut()
+      await this.nav.setRoot("LoginPage");
+      await this.storage.clear();
+      await firebase.auth().signOut();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
-
 }
-
